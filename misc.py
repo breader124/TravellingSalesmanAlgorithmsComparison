01ccba2cfc6e93dc130c8ab9c6c95d31115ec9c6
Michał Wójcik <wojcikmichal98@gmail.com>
2020-03-14 19:14:29 +0100

    Add distance function

--- misc.py
+++ misc.py
@@ -1,8 +1,15 @@
+from math import sqrt
+
+
 class Node:
     def __init__(self, label, x, y):
         self.label = label
         self.x = x
         self.y = y
+
+
+def dist(a: Node, b: Node):
+    return sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
 
 
 def read_data(filename: str):
