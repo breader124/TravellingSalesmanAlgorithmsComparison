916caae63323163ee646accd2502dd9704ec8db2
Michał Wójcik <wojcikmichal98@gmail.com>
2020-03-19 09:23:38 +0100

    Fix heuristics

--- a_star.py
+++ a_star.py
@@ -83,27 +83,32 @@ def chose_home_too_early(state, start_node, total_size):
 
 
 def heuristic(edges, state, nodes):
-    return min_edge_len_between_not_used_nodes(edges, state, nodes) * (len(nodes) - len(state) + 1)
+    span_tree = False
+    if span_tree:
+        return kruskal_algorithm(nodes)
+    else:
+        nodes_left = len(nodes) - len(state) + 1
+        return min_edge_len_between_not_used_nodes(edges, state) * nodes_left
 
 
-# def kruskal_algorithm(nodes):
-#     msp_tree_dist = 0
-#
-#     edges = edges_from_nodes(nodes)
-#
-#     edges.sort(key=take_len)
-#
-#     same_nodes = set()
-#     while edges and len(same_nodes) < len(nodes):
-#         current_edge = edges[0]
-#         if current_edge.first_node not in same_nodes or current_edge.second_node not in same_nodes:
-#             same_nodes.add(current_edge.first_node)
-#             same_nodes.add(current_edge.second_node)
-#             msp_tree_dist = msp_tree_dist + current_edge.length
-#
-#         edges.remove(current_edge)
-#
-#     return msp_tree_dist
+def kruskal_algorithm(nodes):
+    msp_tree_dist = 0
+
+    edges = edges_from_nodes(nodes)
+
+    edges.sort(key=take_len)
+
+    same_nodes = set()
+    while edges and len(same_nodes) < len(nodes):
+        current_edge = edges[0]
+        if current_edge.first_node not in same_nodes or current_edge.second_node not in same_nodes:
+            same_nodes.add(current_edge.first_node)
+            same_nodes.add(current_edge.second_node)
+            msp_tree_dist = msp_tree_dist + current_edge.length
+
+        edges.remove(current_edge)
+
+    return msp_tree_dist
 
 
 def edges_from_nodes(nodes):
@@ -121,21 +126,11 @@ def edges_from_nodes(nodes):
     return edges
 
 
-def min_edge_len_between_not_used_nodes(edges, state, nodes):
-    if one_node_from_path_left(state, nodes):
-        node_left = set(nodes).difference().pop()
-        return min(dist(state[-1], node_left), dist(node_left, nodes[0]))
-
-    elif full_path_done(state, nodes):
-        return dist(state[-1], nodes[0])
-
-    elif at_the_beginning(state, nodes):
-        return 0
-
-    else:
-        for edge in edges:
-            if not node_used(edge, state):
-                return edge.length
+def min_edge_len_between_not_used_nodes(edges, state):
+    for edge in edges:
+        if not edge_used(edge, state):
+            return edge.length
+    return 0.0
 
 
 def one_node_from_path_left(state, nodes):
@@ -150,9 +145,13 @@ def at_the_beginning(state, nodes):
     return len(state) == len(nodes) + 1
 
 
-def node_used(edge, state):
-    state = [s.label for s in state]
-    return (edge.first_node.label in state) or (edge.second_node.label in state)
+def edge_used(edge, state):
+    try:
+        first = state.index(edge.first_node.label)
+        second = state.index(edge.first_node.label)
+        return abs(first - second) == 1
+    except ValueError:
+        return False
 
 
 def take_len(edge):
