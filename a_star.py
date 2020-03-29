bd44db64b793aaf08f465c3a19d48456fd2c39ef
Michał Wójcik <wojcikmichal98@gmail.com>
2020-03-29 18:00:54 +0200

    Another A* implementation

--- a_star.py
+++ a_star.py
@@ -2,39 +2,41 @@ from time import process_time
 import heapq
 
 from heuristic_algorithm import HeuristicAlgorithm
+from misc import dist
 
 
 class AStarAlgorithm(HeuristicAlgorithm):
     def __init__(self, nodes):
         HeuristicAlgorithm.__init__(self, nodes)
         self.states = []
+        heapq.heapify(self.states)
 
     def run(self):
         time_start = process_time()
 
-        heapq.heappush(self.states, (0, self.current_state))
+        current_state = [self.nodes[0]]
+        current_cost = 0
+        nodes_left = self.nodes[1:]
+        nodes_set = set(self.nodes)
 
-        while not self.is_terminal_state():
-            self.update_expanded_state()
-            self.update_states_vector()
-            self.update_current_state()
+        while len(current_state) != len(self.nodes) + 1:
+            nodes_left = nodes_set.difference(current_state)
+            if len(nodes_left) == 0:
+                considered = current_state + [self.nodes[0]]
+                cost = dist(current_state[-1], self.nodes[0])
+                heapq.heappush(self.states, (current_cost + cost, considered))
 
-        path = self.current_state
+            for node in nodes_left:
+                considered = current_state + [node]
+                cost = dist(current_state[-1], node)
+                estimated_remaining = self.heuristic.compute(considered, 'mst')
+                heapq.heappush(self.states, (current_cost + cost + estimated_remaining, considered))
+            current_cost, current_state = heapq.heappop(self.states)
+
+        path = current_state
         cost = self.dist_from_beginning(path)
 
         time_end = process_time()
         time_elapsed = time_end - time_start
 
         return path, cost, time_elapsed
-
-    def update_states_vector(self):
-        heapq.heappop(self.states)
-        for s in self.expanded_state:
-            dist_from_beg = self.dist_from_beginning(s)
-            cost = dist_from_beg + self.heuristic.compute(s)
-            heapq.heappush(self.states, (cost, s))
-
-    def update_current_state(self):
-        popped = heapq.heappop(self.states)
-        self.current_state = popped[1]
-        heapq.heappush(self.states, popped)
