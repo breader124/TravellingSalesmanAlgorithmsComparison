8c140d6aab4b94901bb6d0355fb77b2ab3c36db4
Michał Wójcik <wojcikmichal98@gmail.com>
2020-04-11 19:19:59 +0200

    Fix a-star

--- a_star.py
+++ a_star.py
@@ -20,18 +20,25 @@ class AStarAlgorithm(HeuristicAlgorithm):
         nodes_set = set(self.nodes)
 
         while len(current_state) != len(self.nodes) + 1:
-            nodes_left = nodes_set.difference(current_state)
+            nodes_left = [
+                node
+                for node in self.nodes
+                if node not in current_state
+            ]
+
             if len(nodes_left) == 0:
                 considered = current_state + [self.nodes[0]]
                 cost = dist(current_state[-1], self.nodes[0])
-                heapq.heappush(self.states, (current_cost + cost, considered))
+                new_cost = current_cost + cost
+                heapq.heappush(self.states, (new_cost, (new_cost, considered)))
 
             for node in nodes_left:
                 considered = current_state + [node]
                 cost = dist(current_state[-1], node)
                 estimated_remaining = self.heuristic.compute(considered, 'mst')
-                heapq.heappush(self.states, (current_cost + cost + estimated_remaining, considered))
-            current_cost, current_state = heapq.heappop(self.states)
+                new_cost = current_cost + cost
+                heapq.heappush(self.states, (new_cost + estimated_remaining, (new_cost, considered)))
+            _, (current_cost, current_state) = heapq.heappop(self.states)
 
         path = current_state
         cost = self.dist_from_beginning(path)
